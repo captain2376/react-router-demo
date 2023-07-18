a721c604c62415797ccd26993c91a6a93601293d
captain2376 <b17296@rgukt.ac.in>
2023-07-18 14:44:45 +0530

    18/7/23

--- src/App.js
+++ src/App.js
@@ -3,13 +3,41 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './components/Home';
 import { About } from './components/About';
+import { Navbar } from './components/Navbar';
+import { OrderSummary } from './components/OrderSummary';
+import { PageNotFound } from './components/PageNotFound';
+import { Product } from './components/Product';
+import { FeaturedProducts } from './components/FeaturedProducts';
+import { NewProducts } from './components/NewProducts';
+import { Users } from './components/Users';
+import { UserDetails } from './components/UserDetails';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='About' element={<About/>} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        
+        <Route path='About' element={<About />} />
+        <Route path='order-summary' element={<OrderSummary />} ></Route>
+        <Route path='Product' element={<Product />}>
+        {/* default page */}
+          <Route index element={<FeaturedProducts />} />
+          <Route path='featured' element={<FeaturedProducts />} />
+          <Route path='new' element={<NewProducts />} />
+        </Route >
+        <Route path='*' element={<PageNotFound />} />
+        <Route path='users' element={<Users/>}/>
+        {/* <Route path='/users/1' element={<UserDetails/> }/>
+        <Route path='/users/2' element={<UserDetails/> }/>
+        <Route path='/users/3' element={<UserDetails/> }/> */
+        }
+        <Route path='users/:userId' element={<UserDetails/> } />
+        
+      </Routes>
+
+    </>
   );
 }
 
